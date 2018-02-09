3da369792b13f8d54e51054ec846ec3bf2e86af6
yasui05821 <yasui05821@gmail.com>
2018-02-09 13:02:48 +0900

    HEAD修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,42 @@
 module.exports = {
   /*
-  ** Build configuration
-  */
-  build: {},
+   ** Build configuration
+   */
+    build: {},
   /*
-  ** Headers
-  ** Common headers are already provided by @nuxtjs/pwa preset
-  */
-  head: {},
+   ** Headers
+   ** Common headers are already provided by @nuxtjs/pwa preset
+   */
+    head: {
+        titleTemplate: '株式会社chatbox',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no' },
+            { hid: 'description', name: 'description', content: 'Meta description' },
+        ],
+        link: [
+            { rel: 'shortcut icon', href: '/favicon.ico' },
+            { rel:'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
+        ]
+    },
+    css: [ ],
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress-bar color
+   */
+    loading: { color: '#3B8070' },
   /*
-  ** Customize app manifest
-  */
-  manifest: {
-    theme_color: '#3B8070'
-  },
+   ** Customize app manifest
+   */
+    manifest: {
+        theme_color: '#3B8070'
+    },
   /*
-  ** Modules
-  */
-  modules: [
-    '@nuxtjs/pwa'
-  ]
+   ** Modules
+   */
+    modules: [
+        // '@nuxtjs/pwa'
+    ],
+    plugins: [
+        '~/plugins/vue-smooth-scroll'
+    ]
 }
