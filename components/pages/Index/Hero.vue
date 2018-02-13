89cf81741bb691812545ff1f182aec854a4fc934
yasui05821 <yasui05821@gmail.com>
2018-02-13 12:38:31 +0900

    heroセクション　マークアップ

--- components/pages/Index/Hero.vue
+++ components/pages/Index/Hero.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <div class="p-hero">
-      <div class="p-hero__inner">
-        <div class="p-hero__heading">
-          <h1 class="p-hero__headingTitle">chatboxでの働き方</h1>
-          <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+      <div class="p-hero__backgoundFilter">
+        <div class="p-hero__inner">
+          <div class="p-hero__heading">
+            <h1 class="p-hero__headingTitle">chatboxでの働き方</h1>
+            <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+          </div>
         </div>
       </div>
     </div>
@@ -19,6 +21,8 @@
   @import "~assets/scss/object/component/_title.scss";
 
   .p-hero{
+    &__backgoundFilter {
+    }
     &__inner {
       @include c-container;
     }
