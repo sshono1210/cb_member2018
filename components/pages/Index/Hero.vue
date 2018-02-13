f45aadcdefd7c896c066811ca980255296cfcac0
yasui05821 <yasui05821@gmail.com>
2018-02-13 12:38:43 +0900

    heroセクション　マークアップ

--- components/pages/Index/Hero.vue
+++ components/pages/Index/Hero.vue
@@ -4,7 +4,7 @@
       <div class="p-hero__backgoundFilter">
         <div class="p-hero__inner">
           <div class="p-hero__heading">
-            <h1 class="p-hero__headingTitle">chatboxでの働き方</h1>
+            <h2 class="p-hero__headingTitle">chatboxでの働き方</h2>
             <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
           </div>
         </div>
