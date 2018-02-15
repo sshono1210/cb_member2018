a70ef08ad2b7b264cd1027fca936a4ce8b2054c8
yasui05821 <yasui05821@gmail.com>
2018-02-15 16:32:50 +0900

    heroセクション修正

--- components/pages/Index/Hero.vue
+++ components/pages/Index/Hero.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <div class="p-hero">
-      <div class="p-hero__backgoundFilter">
-        <div class="p-hero__inner">
-          <div class="p-hero__heading">
-            <h2 class="p-hero__headingTitle">chatboxでの働き方</h2>
-            <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
-          </div>
+      <div class="p-hero__inner">
+        <div class="p-hero__heading">
+          <h2 class="p-hero__headingTitle">chatboxでの働き方</h2>
+          <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
         </div>
       </div>
     </div>
@@ -21,32 +19,82 @@
   @import "~assets/scss/object/component/_title.scss";
 
   .p-hero{
-    background: url('/images/member_top.jpg') center / cover no-repeat;
-    height: 700px;
+    background: url('/images/member_top.jpg') center no-repeat;
+    background-size: cover;
     position: relative;
-    &__backgoundFilter {
+    &::before {
+      content: "";
+      display: block;
+      padding-top: 50%;
       background: linear-gradient(90deg, rgba(195,80,79,0.49) 0%, rgba(182,80,78,0.29) 67.45%, rgba(123,5,3,0) 100%);
-      height: 700px;
+      @include desktop {
+        padding-top: 45%;
+      }
+    }
+    &::after {
+      display: inline-block;
+      content: "";
+      width: 2px;
+      height: 70px;
+      background: #c3504f;
+      position: absolute;
+      left: 50%;
+      top: 270px;
+      transform: translateX(-1px);
+      animation: border_sp .5s ease .5s alternate both running;
+      @include desktop() {
+        height: 140px;
+        animation: border_pc .5s ease .5s alternate both running;
+        top: 740px;
+      }
+      @keyframes border_sp {
+        0% {
+          height: 0;
+        }
+        100% {
+          height: 70px;
+        }
+      }
+      @keyframes border_pc {
+        0% {
+          height: 0;
+        }
+        100% {
+          height: 140px;
+        }
+      }
     }
     &__inner {
       @include c-container;
     }
     &__heading {
       color: #FFF;
-      font-weight: bold;
       position: absolute;
-      bottom: 105px;
+      bottom: 15%;
+      @include desktop {
+
+      }
     }
     &__headingTitle {
-      color: #FFF;
+      display: inline-block;
+      margin-bottom: 10px;
+      font-size: 2.5rem;
       font-weight: bold;
-      font-size: 4rem;
-      letter-spacing: 6px;
+      letter-spacing: 3px;
       line-height: 1.5;
+      @include desktop {
+        margin-bottom: 20px;
+        font-size: 4.5rem;
+        letter-spacing: 6px;
+      }
     }
     &__headingIntro {
-      font-weight: bold;
-      letter-spacing: 1px;
+      font-size: 1.4rem;
+      letter-spacing: 2px;
+      @include desktop {
+        font-size: 1.8rem;
+      }
+
     }
   }
 </style>
