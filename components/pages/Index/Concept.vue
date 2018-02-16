7526d31ebee01d3e10904f9b6406c1f1d819989a
yasui05821 <yasui05821@gmail.com>
2018-02-16 18:35:34 +0900

    conseptセクション　PC

--- components/pages/Index/Concept.vue
+++ components/pages/Index/Concept.vue
@@ -31,6 +31,7 @@
   @import "~assets/scss/object/component/_container.scss";
   @import "~assets/scss/object/component/_title.scss";
   .p-concept{
+    margin-top: 80px;
     &__inner {
       @include c-container;
     }
@@ -42,6 +43,71 @@
     }
     &__headingIntro {
 
+    }
+    &__sec01 {
+      position: relative;
+    }
+    &__sec01Img {
+      display: inline-block;
+      width: 55%;
+      background: url("/images/concept_photo01.jpg") center no-repeat;
+      background-size: cover;
+      &::before {
+        content: "";
+        display: block;
+        padding-top: 100%;
+      }
+    }
+    &__sec01Description {
+      display: inline-block;
+      padding: 20px 0 25% 100px;
+      width: 60%;
+      box-sizing: border-box;
+      line-height: 2;
+      position: absolute;
+      right: 0;
+      z-index: 10;
+      background: linear-gradient(left,hsla(0,0%,99%,0),hsla(0,0%,99%,.5) 10%,hsla(0,0%,100%,.8) 20%,hsla(0,0%,100%,.85) 25%,hsla(0,0%,100%,.9) 61%,#fff 80%)
+    }
+    &__sec02 {
+      padding-bottom: 300px;
+      position: relative;
+    }
+    &__sec02Img {
+      display: inline-block;
+      width: 50%;
+      background: url("/images/concept_photo02.jpg") center no-repeat;
+      background-size: cover;
+      position: absolute;
+      top: -90px;
+      right: 0;
+      z-index: 20;
+      &::before {
+        content: "";
+        display: block;
+        padding-top: 70%;
+      }
+    }
+    &__sec02Keywords {
+      display: inline-block;
+      width: 50%;
+      text-align: center;
+      position: absolute;
+      top: -60px;
+      left: 0;
+      background: linear-gradient(top,hsla(0,0%,99%,0),hsla(0,0%,99%,.5) 10%,hsla(0,0%,100%,.8) 20%,hsla(0,0%,100%,.85) 25%,hsla(0,0%,100%,.9) 61%,#fff 80%)
+    }
+    &__sec02Keyword{
+      margin: 30px 1em;
+      display: inline-block;
+      font-weight: bold;
+      vertical-align: top;
+      writing-mode: vertical-rl;
+      color: #C3504F;
+      text-orientation: upright;
+      font-feature-settings: 'pkna';
+      font-size: 1.8rem;
+      letter-spacing: 2px;
     }
 
   }
