811224639fd3d6ba9ccc4d2c5c69004b0e2b036d
shono <s.shosho1210@gmail.com>
2018-02-20 18:31:41 +0900

    サブタイトル＆概要のダミーデータ差し替え

--- components/pages/Index/Hero.vue
+++ components/pages/Index/Hero.vue
@@ -3,7 +3,7 @@
     <div class="p-hero__inner">
       <div class="p-hero__heading">
         <h2 class="p-hero__headingTitle">chatboxでの働き方</h2>
-        <p class="p-hero__headingIntro">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
+        <p class="p-hero__headingIntro">個人とチームの両輪を大切にする</p>
       </div>
     </div>
   </div>
