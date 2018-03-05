7c039f49460cda2ea22f2b9c76836be22baa14fb
Kazukichi <kazukiti201@gmail.com>
2018-03-05 11:31:42 +0900

    スムーススクロール 修正 (#55)

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-header">
+  <div class="p-header" id="top">
     <a href="https://chatbox-inc.com/" target="_blank" class="p-header--triangle"></a>
     <div class="p-header--logoArea">
         <img class="p-header--logoImg" src="/images/logo.svg" alt="">
